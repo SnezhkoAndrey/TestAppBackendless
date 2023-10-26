ff77a23d76e01846de6b5c20cffd6bf7bbabeaff
SnezhkoAndrey <92161597+SnezhkoAndrey@users.noreply.github.com>
2023-10-26 15:29:35 +0300

    create test app for Backendless

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { Route } from "react-router";
+import { Routes, NavLink, Navigate } from "react-router-dom";
+import "./App.css";
+
+import tabs from "./tabs.json";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        {tabs.map((tb) => (
+          <NavLink to={`/${tb.id}`}>{tb.title}</NavLink>
+        ))}
+      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {tabs.map((tb) => {
+            const DummyTable = React.lazy(() => import(`./${tb.path}`));
+            return <Route path={`/${tb.id}`} element={<DummyTable />} />;
+          })}
+
+          <Route path="*" element={<Navigate to={`/${tabs[0].id}`} />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
